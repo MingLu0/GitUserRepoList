70bfcf19e8437ee58c0bcb8f628efc491773b423
Ming Luo <luoming886@gmail.com>
2019-12-16 22:56:41 +1300

    bond up the button and textfield in the mainactivity, created onclicklistner for the btn and a fetchRepos method

--- app/src/main/java/com/example/gituserrepolist/MainActivity.java
+++ app/src/main/java/com/example/gituserrepolist/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.gituserrepolist;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_fetch;
+    private EditText txt_input_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_fetch = findViewById(R.id.btn_fetch);
+        txt_input_name = findViewById(R.id.editText_name);
+
+        btn_fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchRepos(txt_input_name.getText().toString());
+            }
+        });
+
     }
+
+    /**
+     *
+     * @param user
+     */
+    private void fetchRepos(@Nullable @NonNull String user){
+        Toast.makeText(this,user,Toast.LENGTH_SHORT).show();
+    }
+
+
 }
